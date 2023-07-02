f20247f135c1f77d53e8f1b77e179380b03e0570
myageltech <myagel@campus.technion.ac.il>
2023-07-02 18:16:20 +0300

    with 1

---
+++ malloc_1.cpp
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#define MAX_SIZE 100000000
+#define SIZE_LIMIT_CHECK(size) ((size > 0) && (size < MAX_SIZE))
+
+void *smalloc(size_t size){
+    if(!SIZE_LIMIT_CHECK(size)){
+        return NULL;
+    }  
+    void *p = sbrk(0);
+    void *request = sbrk(size);
+    if(request == (void*) -1){
+        return NULL;
+    }else{
+        return p;
+    }
+}
