35f1ce26f541bca2cb29ca4cc1d27574669c43bb
myageltech <myagel@campus.technion.ac.il>
2023-07-06 18:21:30 +0300

    cmakelists

---
+++ build/CMakeFiles/OS4_2.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yagelmaimon/Desktop/technion/operatingsystem/my_hws/hw4/hw4wet/malloc_2.cpp" "CMakeFiles/OS4_2.dir/malloc_2.cpp.o" "gcc" "CMakeFiles/OS4_2.dir/malloc_2.cpp.o.d"
+  "/Users/yagelmaimon/Desktop/technion/operatingsystem/my_hws/hw4/hw4wet/test_2.cpp" "CMakeFiles/OS4_2.dir/test_2.cpp.o" "gcc" "CMakeFiles/OS4_2.dir/test_2.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/OS4_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS4_2.dir/malloc_2.cpp.o"
+  "CMakeFiles/OS4_2.dir/malloc_2.cpp.o.d"
+  "CMakeFiles/OS4_2.dir/test_2.cpp.o"
+  "CMakeFiles/OS4_2.dir/test_2.cpp.o.d"
+  "OS4_2"
+  "OS4_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS4_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
